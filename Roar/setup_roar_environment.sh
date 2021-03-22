5bf11c5c291aa161618606ccb0332331cbce7a2e
Luke Trusel <trusel@gmail.com>
2021-03-22 17:36:47 -0400

    Update setup_roar_environment.sh

--- Roar/setup_roar_environment.sh
+++ Roar/setup_roar_environment.sh
@@ -6,15 +6,19 @@
 
 # load conda
 module load anaconda3/2020.07
+sleep 5
 
 # create new conda environment in the user's work directory (larger than home)
 conda env create -p /storage/work/$USER/conda/envs/geospatial-geog497 -f ~/GEOG497/Roar/geospatial-geog497.yml
+sleep 5
 
 # activate the environment
 conda activate /storage/work/$USER/conda/envs/geospatial-geog497
+sleep 5
 
 # install kernel in /storage/home/l/$USER/.local/share/jupyter/kernels/
 python -m ipykernel install --user --name geospatial-geog497
+sleep 5
 
 # deactivate conda env
 conda deactivate
